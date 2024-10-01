f5397915402ffe3647024401304af81c3b0da3b3
Pallab Rajbongshi <pallabops07@gmail.com>
2024-10-01 23:56:54 +0530

    movie rented store 01/10/24

--- mrs.py
+++ mrs.py
@@ -1,23 +1,15 @@
-from package.catalog_management import addMovie
-from package.rentals import rentMovie
-from package.earnings import rentalEaring
 
-m_list =[]
-rented_movie_list = []
+import catalog
+import rentals 
+import earnings
 
-u_details={}
-def addUser(u_details,c_name):
-    u_details["name"] = c_name
-    print(f"user added : {u_details}")
 
-def allRentalMovie(ren):
-    print("displaying rented movie list :  ")
-    if len(ren) != 0:
-        print(ren)
-    else:
-        print("no one rented any movie ....")
-addMovie(m_list,"HP","comedy",120)
-addUser(u_details,"Pallab")
-rentMovie(m_list,u_details,rented_movie_list,"HP","Pallab")
-rentalEaring(m_list,rented_movie_list)
-allRentalMovie(rented_movie_list)
+catalog.addMovie("avatar","action",120)
+catalog.addMovie("uri","action",120)
+catalog.availableMovies()
+# addUser(u_details,"Pallab")
+# addUser(u_details,"John")
+rentals.rentMovie(catalog.m_list,"avatar","Pallab")
+rentals.rentMovie(catalog.m_list,"uri","Pallab")
+rentals.displayRentedMovies()
+earnings.rentalEaring(rentals.rented_movie_list)
--- package/catalog_management.py
+++
@@ -1,5 +0,0 @@
-
-def addMovie(m_list,title,genre,r_price,):
-    m_details = (title,genre,r_price)
-    m_list.append(m_details)
-    print(f"added movie : {m_list}")
